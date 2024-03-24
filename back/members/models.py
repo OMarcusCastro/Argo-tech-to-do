ec2752cf1e00f08ed79542fd292c4283c6e0d265
Marcus Castro <51335230+OMarcusCastro@users.noreply.github.com>
2024-03-24 12:15:26 -0300

    add: custom user v1

---
+++ back/members/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+# Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise VelueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        if not email:
+            raise VelueError("The Email must be set")
+
+        if not password:
+            raise VelueError("The Password must be set")
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True, max_length=100)
+    username = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return str(self.email)
